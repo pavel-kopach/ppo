d2d8278c9fbd3c2982365db45956840ce4a1a2d0
GabenFreeman <253055@student.pwr.edu.pl>
2020-03-07 23:37:22 +0100

    Added solutions

---
+++ solutions/GabenFreeman9_E1/cmake-build-debug/CMakeFiles/GabenFreeman9.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GabenFreeman9.dir/main.cpp.obj"
+  "GabenFreeman9.exe"
+  "GabenFreeman9.exe.manifest"
+  "GabenFreeman9.lib"
+  "GabenFreeman9.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GabenFreeman9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
